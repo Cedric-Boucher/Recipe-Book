0ae181c2448deb82b47b9923dabe25a9457139c8
Cedric Boucher <onebitalpha@gmail.com>
2024-04-08 21:22:52 -0600

    created recipe template

---
+++ Recipes/JSON/recipe_validator.py
@@ -0,0 +1,8 @@
+# should validate all recipe json files to ensure:
+# 1. structure is valid and contains all required fields
+# 2. structure does not contain unrecognized fields
+# 3. all field values are valid (of the correct type, make sense, etc)
+# 4. all units are valid and recognized
+# 5. picture file names are valid and point to existing pictures in Pictures folder
+
+# create enums for Python for things that should be enums
