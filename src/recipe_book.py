8c76e55b842c8134837711dfa1e4944d41f9e338
Cedric Boucher <onebitalpha@gmail.com>
2024-07-01 20:34:10 -0600

    remove outdated comment

--- src/recipe_book.py
+++ src/recipe_book.py
@@ -1,14 +1,3 @@
-# should validate all recipe json files to ensure:
-# 1. structure is valid and contains all required fields
-# 2. structure does not contain unrecognized fields
-# 3. all field values are valid (of the correct type, make sense, etc)
-# 4. all units are valid and recognized
-# 5. picture file names are valid and point to existing pictures in Pictures folder
-
-# create enums for Python for things that should be enums
-
-# it's not well defined when ingredient names shouldbe singular or plural, ingredients should be enums too (names that only exist in one place)
-
 import sqlite3
 from typing import Any
 import os
