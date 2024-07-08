f99e197e7a7be3dcbac95f9103cb45666f7f1b61
Cedric Boucher <onebitalpha@gmail.com>
2024-07-07 21:50:23 -0600

    add log path to config

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 STORE_IN_MEMORY: bool = False
 
-DATABASE_FILE_PATH: str = "C:/Users/onebi/Documents/GitHub/Recipe-Book/database/recipe_book.sqlite3"
+DATABASE_FILE_PATH: str = "database/recipe_book.sqlite3"
+
+LOG_FILE_PATH: str = "recipe_book.log"
