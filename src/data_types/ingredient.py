480e8f60dfbe7f9a3c6b75631d8ba52bb47ae8d1
Cedric Boucher <onebitalpha@gmail.com>
2024-07-29 22:00:46 -0600

    add missing property to ingredient class

--- src/data_types/ingredient.py
+++ src/data_types/ingredient.py
@@ -7,17 +7,21 @@ class Ingredient():
         self,
         ingredient_type: Ingredient_Type,
         ingredient_brand: Ingredient_Brand,
-        nutrition_info: Nutrition_Info
+        nutrition_info: Nutrition_Info,
+        amount_grams: int
     ) -> None:
         assert (isinstance(ingredient_type, Ingredient_Type))
         assert (len(ingredient_type) > 0)
         assert (isinstance(ingredient_brand, Ingredient_Brand))
         assert (len(ingredient_brand) > 0)
         assert (isinstance(nutrition_info, Nutrition_Info))
+        assert (isinstance(amount_grams, int))
+        assert (amount_grams > 0)
 
         self.__ingredient_type: Ingredient_Type = ingredient_type
         self.__ingredient_brand: Ingredient_Brand = ingredient_brand
         self.__nutrition_info: Nutrition_Info = nutrition_info
+        self.__amount_grams: int = amount_grams
 
     @property
     def ingredient_type(self) -> Ingredient_Type:
@@ -30,3 +34,7 @@ class Ingredient():
     @property
     def nutrition_info(self) -> Nutrition_Info:
         return self.__nutrition_info
+
+    @property
+    def amount_grams(self) -> int:
+        return self.__amount_grams
